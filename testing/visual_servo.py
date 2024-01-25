4e65765ee3116a7c73abb9814d2ef2bbe88aad7a
endrevh <endre.hagestuen@hotmail.com>
2024-01-25 15:26:48 +0100

    Successful visual servo with mediapipe

--- testing/visual_servo.py
+++ testing/visual_servo.py
@@ -52,98 +52,88 @@ def initialize_hands(detection_confidence):
     hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=detection_confidence)
     return hands
 
-# Check if palm is facing towards camera by comparing thumb and pinky using cross product. Change logic if using left hand
-def check_hand_direction(landmarks_camera_coordinates):
-    # Extract coordinates of wrist, thumb tip and pinky tip
-    index_wrist = 0
-    index_thumb_tip = 4
-    index_pinky_tip = 20
-    wrist = np.array(landmarks_camera_coordinates[index_wrist])
-    thumb_tip = np.array(landmarks_camera_coordinates[index_thumb_tip])
-    pinky_tip = np.array(landmarks_camera_coordinates[index_pinky_tip])
+# Check if palm is facing towards camera by cross product
+def check_hand_direction(landmark_coordinates):
+    base_landmark = np.array(landmark_coordinates[0])
+    first_direction = np.array(landmark_coordinates[1])
+    second_direction = np.array(landmark_coordinates[2])
 
-    # Calculate vectors from wrist to pinky tip and from wrist to thumb tip
-    vector_wrist_to_pinky = pinky_tip - wrist
-    vector_wrist_to_thumb = thumb_tip - wrist
+    # Calculate vectors
+    vector1 = second_direction - base_landmark
+    vector2 = first_direction - base_landmark
 
     # Calculate the cross product, only use x and y components of vector in the cross product
-    cross_product = np.cross(vector_wrist_to_pinky[0:2], vector_wrist_to_thumb[0:2])
+    cross_product = np.cross(vector1[0:2], vector2[0:2])
 
     # Determine the sign of the cross product
     if cross_product > 0:
         return True
     else:
+        print("Palm not facing towards camera")
         return False
 
-# Tracking function for human hand using MediaPipe. Use average of all landmarks
-def track_hand(hand_detector, color_image_rgb, depth_frame, camera_matrix, annotate_image):
+# Tracking function for human hand using MediaPipe
+def track_hand(hand_detector, color_image_rgb, depth_frame, camera_matrix, coordinates_at_index, indices_for_hand_direction, annotate_image):
 
     mp_drawing = mp.solutions.drawing_utils
     
     # Process the color image with MediaPipe
     results = hand_detector.process(color_image_rgb)
 
-    landmarks_camera_coordinates = []
+    landmarks_pixel_coordinates = []
+
     if results.multi_hand_landmarks:
         
         # Check handedness
         handedness = Handedness.RIGHT
         if "Right" in str(results.multi_handedness[0].classification): #opposite because of flipped image
             handedness = Handedness.LEFT
+            print("Left hand detected")
 
-        # Convert from pixel to camera coordinates for all landmarks
-        landmarks = results.multi_hand_landmarks[0]            
-        for landmark in landmarks.landmark:
-            
+        # Convert to pixel coordinates for selected landmarks
+        landmarks = results.multi_hand_landmarks[0]   
+        for index in indices_for_hand_direction:
+            landmark = landmarks.landmark[index]
             x_pixel, y_pixel = int(landmark.x * color_image_rgb.shape[1]), int(landmark.y * color_image_rgb.shape[0])
 
             # Sanity check 
             x_pixel = np.clip(x_pixel, 0, color_image_rgb.shape[1] - 1)
             y_pixel = np.clip(y_pixel, 0, color_image_rgb.shape[0] - 1)
-
-            z_camera = depth_frame.get_distance(x_pixel, y_pixel)
-            
-            # Get x,y in camera coordinates using estimated depth and camera parameters
-            x_camera = (x_pixel - camera_matrix[0,2]) * z_camera / camera_matrix[0,0]
-            y_camera = (y_pixel - camera_matrix[1,2]) * z_camera / camera_matrix[1,1]
-
-            landmarks_camera_coordinates.append([x_camera, y_camera, z_camera])
+            landmarks_pixel_coordinates.append([x_pixel, y_pixel])
+        
+        # Check if palm is facing towards camera
+        palm_towards_camera = check_hand_direction(landmarks_pixel_coordinates)
         
         if annotate_image:
             # Draw landmarks on image
             mp_hands = mp.solutions.hands
             mp_drawing.draw_landmarks(color_image_rgb, landmarks, mp_hands.HAND_CONNECTIONS)
 
-        # Check if palm is facing towards camera
-        palm_towards_camera = check_hand_direction(landmarks_camera_coordinates)
+        # Convert to camera coordinates for only one landmark
+        landmark = landmarks.landmark[coordinates_at_index]
+        x_pixel, y_pixel = int(landmark.x * color_image_rgb.shape[1]), int(landmark.y * color_image_rgb.shape[0])
 
-        # Calculate averages
-        sum_x = sum_y = sum_z = 0
+        # Sanity check
+        x_pixel = np.clip(x_pixel, 0, color_image_rgb.shape[1] - 1)
+        y_pixel = np.clip(y_pixel, 0, color_image_rgb.shape[0] - 1)
+        z_camera = depth_frame.get_distance(x_pixel, y_pixel)
+        x_camera = (x_pixel - camera_matrix[0,2]) * z_camera / camera_matrix[0,0]
+        y_camera = (y_pixel - camera_matrix[1,2]) * z_camera / camera_matrix[1,1]
 
-        elements_added = 0
-        for landmark in landmarks_camera_coordinates:
-            if 0.0 in landmark:
-                continue
-            sum_x += landmark[0]
-            sum_y += landmark[1]
-            sum_z += landmark[2]
-            elements_added += 1
+        print(f"Depth at wrist: {z_camera}")
+        camera_coordinates = np.array([x_camera, y_camera, z_camera])
 
-        if elements_added == 0:
+        if 0.0 in camera_coordinates:
             return None, None, None, None
-        
-        average_x = landmarks_camera_coordinates[0][0] #sum_x / elements_added
-        average_y = landmarks_camera_coordinates[0][1] #sum_y / elements_added
-        average_z = landmarks_camera_coordinates[0][2] #sum_z / elements_added
 
-        return np.array([average_x, average_y, average_z]), color_image_rgb, handedness, palm_towards_camera
+        return camera_coordinates, color_image_rgb, handedness, palm_towards_camera
     else:
         print("No hand detected at all")
         return None, None, None, None
 
 # Control robot towards desired position using servo
 def servoL(rtde_c, target_pose, current_pose, time, lookahead_time, scaling_factor):
-    acceleration = 0.1  # these inputs get ignored in the current version of ur_rtde
+    acceleration = 0.1  # these two inputs get ignored in the current version of ur_rtde
     speed = 0.1
     gain = 100  # minimum tolerated value is 100
 
@@ -188,28 +178,6 @@ def transform_vector(vector, transformation_matrix):
 controller_ready_event = threading.Event()
 running_flag = True
 
-# Initialize a thread-safe queue for control commands
-#control_queue = Queue()
-
-# Function that will be executed by the thread
-"""def controller_notify(scheduler):
-    while running_flag:
-        time.sleep(1.0 / 10)
-        controller_ready_event.set()
-        #print("Controller Ready Set")"""
-
-        # Check if there are control commands in the queue
-        #while not control_queue.empty():
-         #   control_command = control_queue.get()
-          #  servoL(*control_command)
-
-# Function that will be executed by the scheduler
-"""def controller_notify(scheduler):
-    controller_ready_event.set()
-    # Reschedule the event
-    if running_flag:
-        scheduler.enter(1.0 / 10, 1, controller_notify, (scheduler,))"""
-
 # Signal handler for Ctrl+C
 def signal_handler(sig, frame):
     global running_flag
@@ -236,25 +204,15 @@ def main():
     # Initialize desired tracking offset
     tracking_offset_setpoint = 0.0
 
-    frequency = 10
+    frequency = 15
     dt = 1.0 / frequency
 
     # initialize camera
-    pipeline = initialize_camera(15)
+    pipeline = initialize_camera(frequency)
 
     # Align the depth frame to color frame
     align_to = rs.stream.color
     align = rs.align(align_to)
-    
-    # Create a scheduler instance
-    #scheduler = sched.scheduler(time.time, time.sleep)
-
-    # Schedule the first event
-    #scheduler.enter(1.0 / 10, 1, controller_notify, (scheduler,))
-
-    # Start the scheduler in a separate thread
-    #scheduler_thread = threading.Thread(target=scheduler.run)
-    #scheduler_thread.start()
 
     # Register the signal handler
     signal.signal(signal.SIGINT, signal_handler)
@@ -262,13 +220,19 @@ def main():
     timestamp = datetime.now().timestamp()
 
     time.sleep(5.0)
+
+    counter = 0
+
+    data_recorded = []
+
     while running_flag:
+        counter += 1
         #if controller_ready_event.wait(0):
             #controller_ready_event.clear()
             #print("Main Thread: Controller Ready is True")
             
         frames = pipeline.wait_for_frames()
-
+        print("-------------------------------")
         # Save timestamp
         #print(f"Diff: {datetime.now().timestamp() - timestamp}")
         timestamp = datetime.now().timestamp()
@@ -283,12 +247,27 @@ def main():
         
         color_image_rgb = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
 
-        hand_coordinates_eye, color_frame_annotated, handedness, palm_towards_camera = track_hand(hand_detector, color_image_rgb, depth_frame, camera_matrix, False)
+        id_wrist = 0
+        id_index_finger = 5
+        id_pinky_finger = 17
+        indices_for_hand_direction = [id_wrist, id_index_finger, id_pinky_finger]
+        
+        save_image = False
+        if counter == 50:
+            save_image = True
+
+        hand_coordinates_eye, color_frame_annotated, handedness, palm_towards_camera = track_hand(hand_detector, color_image_rgb, depth_frame,
+                                                                                                  camera_matrix, id_wrist, indices_for_hand_direction, save_image)
+        if counter == 50:
+            cv2.imwrite("../data/visual_servo_mediapipe_annotated_orange_glove_50.png", cv2.cvtColor(color_frame_annotated,cv2.COLOR_RGB2BGR))
 
         if hand_coordinates_eye is not None and handedness == Handedness.RIGHT and palm_towards_camera: # hand successfully detected
-            #print("detected")
+            print("detected")
 
+            # Get current pose and speed
             flange_pose = rtde_r.getActualTCPPose()
+            flange_speed = rtde_r.getActualTCPSpeed()
+
             flange_tvec = np.array(flange_pose[0:3])
             flange_rvec = np.array(flange_pose[3:6])
 
@@ -312,28 +291,44 @@ def main():
             
             elif tracking_state == TrackingState.TRACKING:
                 flange_tvec_desired = hand_coordinates_base - tracking_offset_setpoint
-                pose_desired = np.hstack((flange_tvec_desired, flange_rvec))  # keep rotation vector unchanged
+                pose_desired = np.hstack((flange_tvec_desired,flange_rvec))  # keep rotation vector unchanged
+
+                # Save data to file
+                timestamp = datetime.now().timestamp()
+
+                line = (f"{timestamp},"
+                f"{flange_pose[0]},{flange_pose[1]},{flange_pose[2]},"
+                f"{flange_pose[3]},{flange_pose[4]},{flange_pose[5]},"
+                f"{flange_speed[0]},{flange_speed[1]},{flange_speed[2]},"
+                f"{flange_speed[3]},{flange_speed[4]},{flange_speed[5]},"
+                f"{hand_coordinates_base[0]},{hand_coordinates_base[1]},{hand_coordinates_base[2]},"
+                f"{pose_desired[0]},{pose_desired[1]},{pose_desired[2]},"
+                f"{pose_desired[3]},{pose_desired[4]},{pose_desired[5]},"
+                f"{tracking_offset_setpoint[0]},{tracking_offset_setpoint[1]},{tracking_offset_setpoint[2]}\n")
+
+                data_recorded.append(line)
 
                 lookahead_time = 0.1
-                scaling_factor = 0.04
+                scaling_factor = 0.5
 
                 # Only block the robot for remaining time of dt
                 elapsed = datetime.now().timestamp() - timestamp
                 servotime = dt - elapsed
 
-                #servoL(rtde_c, pose_desired, flange_pose, servotime, lookahead_time, scaling_factor)
+                servoL(rtde_c, pose_desired, flange_pose, servotime, lookahead_time, scaling_factor)
                 #control_queue.put((rtde_c, pose_desired, flange_pose, servotime, lookahead_time, scaling_factor))
                 #print(f"Desired pose: {pose_desired}")
                 #print(f"Current pose: {flange_pose}")
                 #print(f"Hand pose: {hand_coordinates_base}")
 
                 #print(f"Setpoint: {tracking_offset_setpoint}")
-                print(f"dt {dt}")
-                print(f"elapsed {elapsed}")
-                print(f"servotime {servotime}")
+                #print(f"dt {dt}")
+                #print(f"elapsed {elapsed}")
+                #print(f"servotime {servotime}")
 
         else:
-            print("Not detected")
+            #print("Not detected")
+            data_recorded.append("NOT DETECTED\n")
             if tracking_state == TrackingState.SEARCHING:
                 continue
             elif tracking_state == TrackingState.TRACKING:
@@ -341,6 +336,12 @@ def main():
                 #rtde_c.speedStop(1.0)
                 print("Stopped")
                 tracking_state = TrackingState.SEARCHING
+
+
+    # Open data file and save data
+    with open(f'../data/visual_servo_mediapipe_orange_glove_50.txt', 'w') as file:
+        for line in data_recorded:
+            file.write(line)
 
     # Wait for threads to finish
     #scheduler_thread.join()
